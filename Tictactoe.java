2a51afbb3f1ffc80b946328ccf103f245ec4a1d0
kishor-23 <75676692+kishor-23@users.noreply.github.com>
2022-10-20 03:40:35 -0700

    Update Tictactoe.java

--- Tictactoe.java
+++ Tictactoe.java
@@ -11,6 +11,15 @@ gameBoard is a 5X5 2D character array full of hyphen [ - ], vertical bars [ | ],
          [0][0] =1, [0][2]=2 , [0][4]=3
          [2][0] =4 , [2][2]=5 , [2][4]=6
          [4][0]= 7 , [2][2]=8 , [2][4]=9
+   created functions:
+     1)  printGameBoard(char[][] board)- used to print the game board. 
+     2)  isAvailable(char board[][] ,int pos )- used to check the position is available or not. returns true if available else false.
+     3)  placepiece(char[][] gameBoard,int pos,String usr)- is used to place the piece in the board 
+            The player will be represented by the symbal 'X' 
+            The computer will be represented by the symbal 'O'  
+     4)  checkwinner(char[][] board,char symbol)- checks player/computer wins the game or not. return true if wins else returns false.
+            Winning : Whenever any of the two players has fully filled one row/ column/ diagonal with his symbol (X/ O), he wins and the game ends.
+     5)  isGameOver(char[][] board) - checks whether the game board is full or not , returns true if the board is full of values else false.
 	 
          
 */
@@ -43,7 +52,7 @@ public class Tictactoe {
 			System.out.println("enter the place(1-9):");
 			int player_pos=sc.nextInt();
 			
-			while(!isValid(gameBoard,player_pos)) {
+			while(!isAvailable(gameBoard,player_pos)) {
 				System.out.println(player_pos+"not available");
 				System.out.println("enter the place(1-9):");
 				player_pos=sc.nextInt();
@@ -59,7 +68,7 @@ public class Tictactoe {
 			 }
 			
 			int cpu_pos=rand.nextInt(9)+1;
-			while(!isValid(gameBoard,cpu_pos)) {
+			while(!isAvailable(gameBoard,cpu_pos)) {
 				cpu_pos=rand.nextInt(9)+1;
 			}
 			placepiece(gameBoard,cpu_pos,"cpu");
@@ -89,17 +98,54 @@ public class Tictactoe {
 		
 	}
 	
-  //print game board
-	public static void printGameBoard(char[][] a) {
-		for (char[] i : a) {
+//print game board
+	
+	public static void printGameBoard(char[][] Board) {
+		for (char[] i : Board) {
 			for(char c:i) {
 				System.out.print(c+" ");
 			}
 			System.out.println();	
 		}
 	 }
+
+ //check the place is empty/available	
+    	public static boolean isAvailable(char board[][],int pos )  {
+    		switch(pos) {
+    		case 1:
+    			 return (board[0][0]==' ');  //returns true if space available  			
+    		case 2:
+    			return (board[0][2]==' ');
+    		
+    		case 3:
+    			return (board[0][4]==' ');
+    		
+    		case 4:
+    			return (board[2][0]==' ');
+    		
+    		case 5:
+    			return (board[2][2]==' ');
+    		
+    		case 6:
+    			return (board[2][4]==' ');
+    			
+    	        case 7:
+    		       return (board[4][0]==' ');
+    		
+    	        case 8:
+    		       return ( board[4][2]==' ');
+    		 
+    	        case 9:
+    	               return (board[4][4]==' ');
+    	        default:
+    	    	    return true;
+    	    
+    		}
+    	}
 	
- //place piece
+
+	
+ //place the  piece
     public static void placepiece(char[][] gameBoard,int pos,String usr) {
     	char symbol=' ';
     	if(usr.equals("player")) {
@@ -139,41 +185,35 @@ public class Tictactoe {
 	  }
     }
 	
-    //check the place is empty	
-    	public static boolean isValid (char board[][],int pos )  {
-    		switch(pos) {
-    		case 1:
-    			 return (board[0][0]==' ');  //returns true if space available  			
-    		case 2:
-    			return (board[0][2]==' ');
-    		
-    		case 3:
-    			return (board[0][4]==' ');
-    		
-    		case 4:
-    			return (board[2][0]==' ');
-    		
-    		case 5:
-    			return (board[2][2]==' ');
-    		
-    		case 6:
-    			return (board[2][4]==' ');
-    			
-    	        case 7:
-    		       return (board[4][0]==' ');
-    		
-    	        case 8:
-    		       return ( board[4][2]==' ');
-    		 
-    	        case 9:
-    	               return (board[4][4]==' ');
-    	        default:
-    	    	    return true;
-    	    
+	
+ //check winner
+    	public static boolean checkwinner(char[][] board,char symbol) {
+    		if ((board[0][0] == symbol && board [0][2] == symbol && board [0][4] == symbol) ||
+    		    (board[2][0] == symbol && board [2][2] == symbol && board [2][4] == symbol) ||
+    		    (board[4][0] == symbol && board [4][2] == symbol && board [4][4] == symbol) ){
+    			//row
+    			return true;
     		}
+    				
+    		else if((board[0][0] == symbol && board [2][0] == symbol && board [4][0] == symbol) ||
+    		        (board[0][2] == symbol && board [2][2] == symbol && board [4][2] == symbol) ||
+    			(board[0][4] == symbol && board [2][4] == symbol && board [4][4] == symbol) ) {
+    			//column
+    			return true;
+    		}
+    				
+    		else if	((board[0][0] == symbol && board [2][2] == symbol && board [4][4] == symbol) ||
+    		         (board[0][4] == symbol && board [2][2] == symbol && board [4][0] == symbol) ) {
+    			//cross
+    				return true;
+    			}
+    		else {
+    			return false;
+    		}  		
     	}
+   
  
-        //check game over
+   //check game over
     	public static boolean isGameOver(char[][] b) {
     	   int c=0;
     	    for(int i=0;i<b.length;i++) {
@@ -186,31 +226,7 @@ public class Tictactoe {
     	    	return true;
     	}
 	
-    	//check winner
-    	public static boolean checkwinner(char[][] board,char symbol) {
-    		if ((board[0][0] == symbol && board [0][2] == symbol && board [0][4] == symbol) ||
-    				(board[2][0] == symbol && board [2][2] == symbol && board [2][4] == symbol) ||
-    				(board[4][0] == symbol && board [4][2] == symbol && board [4][4] == symbol) ){
-    			//row
-    			return true;
-    		}
-    				
-    		else if((board[0][0] == symbol && board [2][0] == symbol && board [4][0] == symbol) ||
-    				(board[0][2] == symbol && board [2][2] == symbol && board [4][2] == symbol) ||
-    				(board[0][4] == symbol && board [2][4] == symbol && board [4][4] == symbol) ) {
-    			//column
-    			return true;
-    		}
-    				
-    		else if	((board[0][0] == symbol && board [2][2] == symbol && board [4][4] == symbol) ||
-    				(board[0][4] == symbol && board [2][2] == symbol && board [4][0] == symbol) ) {
-    			//cross
-    				return true;
-    			}
-    		else {
-    			return false;
-    		}  		
-    	}
+    	
 }
 
     
